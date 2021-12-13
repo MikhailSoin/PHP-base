76bd01225033c64a8dd557e27bc0d639d1cfbf55
MikhailSoin <79539901+MikhailSoin@users.noreply.github.com>
2021-12-13 20:25:00 +0300

    test.php
    Тестовый файл

---
+++ test.php
@@ -0,0 +1,32 @@
+<h1>Test</h1>
+<?php
+//    echo "<h1>Hello world! Date".date("d.m.Y")."</h1>";
+?>    
+
+
+<!--<h1>Hello world! Date <?php echo date("d.m.Y")?> </h1>-->
+
+<!--<h1>Hello world!!! Date <?= date("d.m.Y")?> </h1>-->
+<?php
+$name = "GeekBrains user";
+echo "Hello, $name!";
+?>
+
+
+
+<?php
+define('MY_CONST', 100);
+echo MY_CONST;
+?>
+
+
+<?php
+$int10 = 42;
+$int2 = 0b101010;
+$int8 = 052;
+$int16 = 0x2A;
+echo "Десятеричная система $int10 <br>";
+echo "Двоичная система $int2 <br>";
+echo "Восьмеричная система $int8 <br>";
+echo "Шестнадцатеричная система $int16 <br>";
+?>
